1ec44c2d530ec2896a9e3243c35ff19408aa7c16
46989075y <ricardhuelamo@gmail.com>
2016-10-05 16:53:02 +0200

    Iniciar

---
+++ src/correccine/CorrecCine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correccine;
+
+/**
+ *
+ * @author 46989075y
+ */
+public class CorrecCine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
